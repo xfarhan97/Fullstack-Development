9a96abedaa3800ac7af44da54ffdf53219332b4a
xfarhan97 <xfarhan97@gmail.com>
2026-03-03 16:02:17 +0500

    Update 4 02

--- Fullstack Developoment/Tasks/javascript-Project/To Do App/script.js
+++ Fullstack Developoment/Tasks/javascript-Project/To Do App/script.js
@@ -1,26 +1,28 @@
 const taskInput = document.getElementById("taskInput");
 const taskList = document.getElementById("taskList");
-const error = document.getElementById("error");
+const errorMessage = document.getElementById("errorMessage");
 
-// Add task
+taskInput.addEventListener("input", function(){
+    if(taskInput.value.trim() !== ""){
+        errorMessage.textContent = "";
+    }
+});
+
 function addTask(){
-
+    
     let taskText = taskInput.value.trim();
 
     if(taskText === ""){
-        error.textContent = "Task cannot be empty.";
+        errorMessage.textContent = "⚠ Task cannot be empty!";
         return false;
     }
 
-    error.textContent = "";
-
     const li = document.createElement("li");
-
     li.innerHTML = `
         <span>${taskText}</span>
-        <div class="actions">
-            <button class="complete" onclick="toggleComplete(this)">✔</button>
-            <button class="delete" onclick="deleteTask(this)">✖</button>
+        <div class="task-buttons">
+            <button onclick="completeTask(this)">✔</button>
+            <button onclick="deleteTask(this)">✖</button>
         </div>
     `;
 
@@ -28,26 +30,10 @@ function addTask(){
     taskInput.value = "";
 }
 
-// Delete task
 function deleteTask(button){
     button.parentElement.parentElement.remove();
 }
 
-// Mark complete
-function toggleComplete(button){
+function completeTask(button){
     button.parentElement.parentElement.classList.toggle("completed");
 }
-
-// Press Enter to add
-taskInput.addEventListener("keypress", function(e){
-    if(e.key === "Enter"){
-        addTask();
-    }
-});
-
-// Real-time validation
-taskInput.addEventListener("input", function(){
-    if(taskInput.value.trim() !== ""){
-        error.textContent = "";
-    }
-});
